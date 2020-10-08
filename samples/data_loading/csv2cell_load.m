1aacf335185c6a27c61703ca700362fa38862fc1
katodix <darko.katovic@gmail.com>
2020-10-08 12:08:27 +0200

    load examples

---
+++ samples/data_loading/csv2cell_load.m
@@ -0,0 +1,21 @@
+# pkg install -forge io
+# pkg install -forge statistics
+# pkg install -forge nan
+# pkg install -forge dataframe
+
+pkg load io statistics nan dataframe
+
+data = csv2cell('Pejcic_318.csv')
+data(1, :)
+data(2:end, :)
+
+mean(data)
+# median(data)
+# mode(data)
+# range(data)
+# var(data)
+# std(data)
+# skewness(data)
+# kurtosis(data)
+
+# statistics(data)
--- samples/data_loading/dataframe_load.m
+++ samples/data_loading/dataframe_load.m
@@ -11,31 +11,8 @@
 
 pkg load io statistics nan dataframe
 
-addpath('../../../externals/Core.Math.Samples/data')
+# addpath('/Users/katodix/Projects/Samples.MatlabOctave/samples/data_loading')
 
-# data = csv2cell('Pejcic_318.csv')
-# data(1, :) %
-# data(2:end, :) %
-
-# dlmread ('Pejcic_318.csv',',',1,5)
-
-# csvread('Pejcic_318.csv')
-# data = csvread('Pejcic_318.csv')
-
-mean(data)
-median(data)
-mode(data)
-range(data)
-var(data)
-std(data)
-skewness(data)
-kurtosis(data)
-
-statistics(data)
-
-#############
-# DataFrame #
-#############
 data = dataframe('Pejcic_318.csv')
 
 # prvi arg.: od kojeg do kojeg retka; drugi arg.: koje varijable
@@ -54,3 +31,14 @@ summary(df1)
 df2 = data(1:318, ["ATV"; "ATT"])
 mean(df.ATV)
 statistics(df.ATV)
+
+mean(data)
+# median(data)
+# mode(data)
+# range(data)
+# var(data)
+# std(data)
+# skewness(data)
+# kurtosis(data)
+
+# statistics(data)
